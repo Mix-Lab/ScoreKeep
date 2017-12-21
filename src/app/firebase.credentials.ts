493d9d3a13d53b1d17599204de9cbafc42e48830
Mayooran <mayooran99@gmail.com>
2017-12-21 17:08:34 +0530

    Initial Commit

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAUttAlJwl97F8vfV7Xc6CS1IT9zfbDlNM",
+    authDomain: "score-it-b26ab.firebaseapp.com",
+    databaseURL: "https://score-it-b26ab.firebaseio.com",
+    projectId: "score-it-b26ab",
+    storageBucket: "",
+    messagingSenderId: "613444828124"
+};
